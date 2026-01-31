b57f923c6c95622b8c6bbf4320486dfccb4fd00e
misstickles <jofaircloth@gmail.com>
2026-01-31 21:20:22 +0000

    New API projects

---
+++ api/JoF.Imparta.TaskList.Api.Test/Controllers/WeatherShould.cs
@@ -0,0 +1,13 @@
+namespace JoF.Imparta.TaskList.Api.Test.Controllers;
+
+using Shouldly;
+
+[TestClass]
+public sealed class WeatherShould
+{
+    [TestMethod]
+    public async Task Work()
+    {
+        "test".ShouldBe("test");
+    }
+}
